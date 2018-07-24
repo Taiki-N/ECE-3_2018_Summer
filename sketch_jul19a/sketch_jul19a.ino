03494e842eb1597be4ab95f33e2b723b85aed0fc
Taiki <toaru.dokokano.syounen.0610@gmail.com>
2018-07-24 14:49:07 -0700

    Analog pins added

--- sketch_jul19a/sketch_jul19a.ino
+++ sketch_jul19a/sketch_jul19a.ino
@@ -1,7 +1,21 @@
+// Pins
+constexpr unsigned char leftMotorControlPin = 3;
+constexpr unsigned char rightMotorControlPin = 5;
+
+constexpr unsigned char photoTranPin1 = A0;
+constexpr unsigned char photoTranPin2 = A1;
+constexpr unsigned char photoTranPin3 = A2;
+constexpr unsigned char photoTranPin4 = A3;
+
+
 void setup()
 {
-	pinMode(3, OUTPUT);
-	pinMode(5, OUTPUT);
+	pinMode(leftMotorControlPin, OUTPUT);
+	pinMode(rightMotorControlPin, OUTPUT);
+
+	pinMode(photoTranPin1, INPUT);
+
+	Serial.begin(9600);
 }
 
 void loop()
@@ -9,7 +23,11 @@ void loop()
 	unsigned char leftMotorPwm = 230;
 	unsigned char rightMotorPwm = 255;
 
-	analogWrite(3, leftMotorPwm);		// Left motor
-	analogWrite(5, rightMotorPwm);		// Right motor
-}
+	// Sensors
+	short photoTran1 = analogRead(photoTranPin1);
+	Serial.println(photoTran1);
 
+	// PWM Control
+	analogWrite(leftMotorControlPin, leftMotorPwm);			// Left motor
+	analogWrite(rightMotorControlPin, rightMotorPwm);		// Right motor
+}
